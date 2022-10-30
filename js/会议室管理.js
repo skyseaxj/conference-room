fc8ec51736cee983baf223b5ad4d27570998d75a
B20031526徐家骜 <939204858@qq.com>
2022-10-30 18:04:13 +0800

    会议室管理界面和主界面
    为主界面侧边菜单添加点击效果，为会议室管理界面添加读取会议室信息并添加表格信息

--- js/会议室管理.js
+++ js/会议室管理.js
@@ -13,21 +13,17 @@ $(function () {
         site: "五楼",
         num: "300"
     }];
-    // 导航栏点击移除其他选项的active类，给点击的选项添加active类
-    $('.nav>.nav_item').click(function () {
-        // 选取点击对象添加类，再选取其兄弟对象移除类
-        $(this).addClass('active').siblings('li').removeClass('active');
-    });
     // 添加获取数据已有的会议室信息到页面表格中
     function get_meetingRoom() {
        
-        for (var i = 0; i < data.length; i++) {
-            var $tr = $("<tr><td>" + data[i]['name'] +
-            "</td><td>" + data[i]['site'] + 
-            "</td><td>" + data[i]['num'] + 
+        for (var i = 0; i < meeting_room.length; i++) {
+            var $tr = $("<tr><td>" + meeting_room[i]['name'] +
+            "</td><td>" + meeting_room[i]['site'] + 
+            "</td><td>" + meeting_room[i]['num'] + 
             "</td><td>" +'<button type="button" class="btn btn-info">修改</button>'+
-            "</td><td>" +'<button type="button" class="btn btn-danger">删除</button>'+
+            '<button type="button" class="btn btn-danger del" width>删除</button>'+
             "</td></tr>");
+            console.log($tr)
             //把创建出来的$tr追加到tbody中去.
             $('#meetingRoomItem').append($tr);
         }
