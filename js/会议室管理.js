c992de14a06329658dfd0493c271d72202561a6f
B20031526徐家骜 <939204858@qq.com>
2022-10-30 23:28:14 +0800

    会议室管理界面
    为删除按钮添加删除表格元素功能，添加添加会议室功能，通过弹窗输入数据，并且添加到表格中

--- js/会议室管理.js
+++ js/会议室管理.js
@@ -12,7 +12,7 @@ $(function () {
         name: "4号会议室",
         site: "五楼",
         num: "300"
-    }];
+    },];
     // 添加获取数据已有的会议室信息到页面表格中
     function get_meetingRoom() {
        
@@ -20,13 +20,60 @@ $(function () {
             var $tr = $("<tr><td>" + meeting_room[i]['name'] +
             "</td><td>" + meeting_room[i]['site'] + 
             "</td><td>" + meeting_room[i]['num'] + 
-            "</td><td>" +'<button type="button" class="btn btn-info">修改</button>'+
-            '<button type="button" class="btn btn-danger del" width>删除</button>'+
+            "</td><td>" +'<input type="button" class="btn btn-info" value="修改">'+
+            '<input type="button" class="btn btn-danger del" value="删除">'+
             "</td></tr>");
-            console.log($tr)
             //把创建出来的$tr追加到tbody中去.
             $('#meetingRoomItem').append($tr);
         }
     }
     get_meetingRoom();
+
+    // 表格删除操作
+    $('#meetingRoomItem .del').click(function(){
+        // 删除父元素的父元素
+        $(this).parent().parent().remove();
+    })
+// 打开添加界面
+    $('#add').click(function(){
+        // 打开添加界面
+        $('#j_formAdd').show();
+        // 打开遮罩层
+        $('#j_mask').show();
+    })
+    // 关闭添加界面
+    $('#j_hideFormAdd').click(function(){
+        // 关闭添加界面
+        $('#j_formAdd').hide();
+        // 关闭遮罩层
+        $('#j_mask').hide();
+    })
+
+    // 添加界面添加数据
+    $('#j_btnAdd').click(function(){
+        // 获取用户输入内容
+        var txtName=$('#j_name').val();
+        var txtSite=$('#j_site').val();
+        var txtNum=$('#j_num').val();
+        // 把数据添加到会议室信息里
+        meeting_room.push({'name':txtName,site:txtSite,num: txtNum});
+        var $tr = $("<tr><td>" + txtName+
+            "</td><td>" + txtSite + 
+            "</td><td>" + txtNum + 
+            "</td><td>" +'<input type="button" class="btn btn-info" value="修改">'+
+            '<input type="button" class="btn btn-danger del" value="删除">'+
+            "</td></tr>");
+
+        // 为新的删除按钮添加删除事件
+        $tr.find('.del').click(function(){
+            $tr.remove();
+        })
+        // 将信息添加到表格当中显示
+        $('#meetingRoomItem').append($tr);
+        //数据面板和遮罩层隐藏
+         // 关闭添加界面
+         $('#j_formAdd').hide();
+         // 关闭遮罩层
+         $('#j_mask').hide();
+    })
 })
