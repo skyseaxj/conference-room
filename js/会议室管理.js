440e7744f583d0e70878c5f170c74f0468956395
B20031526徐家骜 <939204858@qq.com>
2022-11-01 23:11:09 +0800

    会议室系统
    添加axja

--- js/会议室管理.js
+++ js/会议室管理.js
@@ -3,46 +3,70 @@ $(function () {
     var meeting_room = [{
         name: "2号会议室",
         site: "五楼",
-        num: "100"
+        num: "100",
+        time: [true, true, true]
     }, {
         name: "3号会议室",
         site: "五楼",
-        num: "200"
+        num: "200",
+        time: [true, false, true]
     }, {
         name: "4号会议室",
         site: "五楼",
-        num: "300"
+        num: "300",
+        time1:1 
     },];
+
     // 添加获取数据已有的会议室信息到页面表格中
     function get_meetingRoom() {
-       
+        // 发起Ajax请求会议室信息数据
+        $.get('',function(res){
+            // 判断是否获取成功
+            if(res.staus!=200)return alert('获取会议室信息失败');
+            var spring="";
         for (var i = 0; i < meeting_room.length; i++) {
-            var $tr = $("<tr><td>" + meeting_room[i]['name'] +
-            "</td><td>" + meeting_room[i]['site'] + 
-            "</td><td>" + meeting_room[i]['num'] + 
-            "</td><td>" +'<input type="button" class="btn btn-info" value="修改">'+
-            '<input type="button" class="btn btn-danger del" value="删除">'+
-            "</td></tr>");
+            string = string+"<tr><td>" + meeting_room[i]['name'] +
+                "</td><td>" + meeting_room[i]['site'] +
+                "</td><td>" + meeting_room[i]['num'] +
+                "</td><td>"
+            if (meeting_room[i]['time'][0]) string = string + "早上";
+            if (meeting_room[i]['time'][1]) string = string + "下午";
+            if (meeting_room[i]['time'][2]) string = string + "晚上";
+            string = string + "</td><td>" + '<input type="button" class="btn btn-info" value="修改">' +
+                '<input type="button" class="btn btn-danger del" value="删除" data-id="'+item.id+'">' +
+                "</td></tr>";
+            var $tr = $(string);
             //把创建出来的$tr追加到tbody中去.
-            $('#meetingRoomItem').append($tr);
+            $('#meetingRoomItem').empty().append($tr);
         }
+    })
     }
     get_meetingRoom();
 
-    // 表格删除操作
-    $('#meetingRoomItem .del').click(function(){
-        // 删除父元素的父元素
-        $(this).parent().parent().remove();
+    // 表格删除操作,需要通过父类代理事件才能绑定
+    $('#meetingRoomItem').on('click','.del',function () {
+        // 获取被删除数据的id值
+        var id=$(this).attr('data-id')
+        $ajax({
+            type:'',//请求方式
+            url:'',
+            data:{},
+            success:function(res){
+                if(res.staus!=200)return alert('删除信息信息失败');
+                // 重新获取会议室列表
+                get_meetingRoom();
+            }
+        })
     })
-// 打开添加界面
-    $('#add').click(function(){
+    // 打开添加界面
+    $('#add').click(function () {
         // 打开添加界面
         $('#j_formAdd').show();
         // 打开遮罩层
         $('#j_mask').show();
     })
     // 关闭添加界面
-    $('#j_hideFormAdd').click(function(){
+    $('#j_hideFormAdd').click(function () {
         // 关闭添加界面
         $('#j_formAdd').hide();
         // 关闭遮罩层
@@ -50,30 +74,54 @@ $(function () {
     })
 
     // 添加界面添加数据
-    $('#j_btnAdd').click(function(){
-        // 获取用户输入内容
-        var txtName=$('#j_name').val();
-        var txtSite=$('#j_site').val();
-        var txtNum=$('#j_num').val();
+    $('#j_btnAdd').on('click',function () {
+        // 获取用户输入内容,trim为去除空格
+        var txtName = $('#j_name').val().trim();
+        var txtSite = $('#j_site').val().trim();
+        var txtNum = $('#j_num').val().trim();
+        var time = [$('#j_time1').prop('checked'), $('#j_time2').prop('checked'), $('#j_time3').prop('checked')];
+        console.log(time);
+
+        // 如果填写内容有为空的弹出提示
+        if(txtName.length<=0||txtSite.length<=0||txtNum.length<=0||(!time[0]&&time[1]&&time[2])){
+            return alert('请填写完整的会议室信息');
+        }
+        
+        // 向服务器提交数据
+        $ajax({
+            type:'',//请求方式
+            url:'',
+            data:{},
+            success:function(res){
+                if(res.staus!=200)return alert('添加失败');
+                // 重新获取会议室列表
+                get_meetingRoom();
+            }
+        })
         // 把数据添加到会议室信息里
-        meeting_room.push({'name':txtName,site:txtSite,num: txtNum});
-        var $tr = $("<tr><td>" + txtName+
-            "</td><td>" + txtSite + 
-            "</td><td>" + txtNum + 
-            "</td><td>" +'<input type="button" class="btn btn-info" value="修改">'+
-            '<input type="button" class="btn btn-danger del" value="删除">'+
-            "</td></tr>");
+        meeting_room.push({ 'name': txtName, site: txtSite, num: txtNum });
+        var string = "<tr><td>" + txtName +
+            "</td><td>" + txtSite +
+            "</td><td>" + txtNum +
+            "</td><td>"
+        if (time[0] == true) string = string + "早上";
+        if (time[1] == true) string = string + "下午";
+        if (time[2] == true) string = string + "晚上";
+        string = string + "</td><td>" + '<input type="button" class="btn btn-info" value="修改">' +
+            '<input type="button" class="btn btn-danger del" value="删除">' +
+            "</td></tr>";
+        var $tr = $(string);
 
         // 为新的删除按钮添加删除事件
-        $tr.find('.del').click(function(){
+        $tr.find('.del').click(function () {
             $tr.remove();
         })
         // 将信息添加到表格当中显示
         $('#meetingRoomItem').append($tr);
         //数据面板和遮罩层隐藏
-         // 关闭添加界面
-         $('#j_formAdd').hide();
-         // 关闭遮罩层
-         $('#j_mask').hide();
+        // 关闭添加界面
+        $('#j_formAdd').hide();
+        // 关闭遮罩层
+        $('#j_mask').hide();
     })
 })
--- js/预约信息.js
+++ js/预约信息.js
@@ -1,41 +1,40 @@
 $(function () {
     //预约数据，模拟后端拿到数据
-    var reserve= [{
+    var reserve = [{
         name: "张三",
         num: "会议室2",
         date: "2022-11-2",
-        start:"8:00",
-        end:"10:00",
-        tele:"12345678"
+        time: "晚上",
+        tele: "12345678"
     }, {
         name: "赵四",
         num: "会议室3",
         date: "2022-11-3",
-        start:"8:00",
-        end:"10:00",
-        tele:"12345678"
+        time: "早上",
+        tele: "12345678"
     }, {
         name: "李五",
         num: "会议室4",
         date: "2022-11-4",
-        start:"8:00",
-        end:"10:00",
-        tele:"12345678"
+        time: "下午",
+        tele: "12345678"
     }];
     // 添加获取数据已有的预约信息到页面表格中
     function get_reserve() {
-       
-        for (var i = 0; i < reserve.length; i++) {
-            var $tr = $("<tr><td>" + reserve[i]['name'] +
-            "</td><td>" + reserve[i]['num'] + 
-            "</td><td>" + reserve[i]['date'] + 
-            "</td><td>" + reserve[i]['start'] +
-            "</td><td>" + reserve[i]['end'] +
-            "</td><td>" + reserve[i]['tele'] +
-            "</td></tr>");
-            //把创建出来的$tr追加到tbody中去.
-            $('#reserveItem').append($tr);
-        }
+        //发起ajax请求预约信息数据
+        $.get('http', function (res) {
+
+            for (var i = 0; i < reserve.length; i++) {
+                var $tr = $("<tr><td>" + reserve[i]['name'] +
+                    "</td><td>" + reserve[i]['num'] +
+                    "</td><td>" + reserve[i]['date'] +
+                    "</td><td>" + reserve[i]['time'] +
+                    "</td><td>" + reserve[i]['tele'] +
+                    "</td></tr>");
+                //把创建出来的$tr追加到tbody中去.
+                $('#reserveItem').append($tr);
+            }
+        })
     }
     get_reserve();
 })
