74a0c122061fe97de889d5f68808961bb9a37e2f
B20031527黄伟实 <2318884142@qq.com>
2022-11-02 15:44:47 +0800

    Update 会议室管理.js

--- js/会议室管理.js
+++ js/会议室管理.js
@@ -20,20 +20,26 @@ $(function () {
     // 添加获取数据已有的会议室信息到页面表格中
     function get_meetingRoom() {
         // 发起Ajax请求会议室信息数据
-        $.get('',function(res){
-            // 判断是否获取成功
-            if(res.staus!=200)return alert('获取会议室信息失败');
-            var spring="";
-        for (var i = 0; i < meeting_room.length; i++) {
-            string = string+"<tr><td>" + meeting_room[i]['name'] +
-                "</td><td>" + meeting_room[i]['site'] +
-                "</td><td>" + meeting_room[i]['num'] +
+        $.get('http://localhost/rooms',function(res){
+            // 判断是否获取成功     
+            console.log(res);
+            console.log(res.data.rooms.length);
+         
+            
+
+            if(!res.success)return alert('获取会议室信息失败');
+            var string="";
+        for (var i = 0; i <res.data.rooms.length; i++) {
+            string = string+"<tr><td>" +res.data.rooms[i]['name'] +
+                "</td><td>" +res.data.rooms[i]['location'] +
+                "</td><td>" +res.data.rooms[i]['num'] +
                 "</td><td>"
-            if (meeting_room[i]['time'][0]) string = string + "早上";
-            if (meeting_room[i]['time'][1]) string = string + "下午";
-            if (meeting_room[i]['time'][2]) string = string + "晚上";
+                var times=res.data.rooms[i]['times'].split("");
+            if (times[0]) string = string + "早上";
+            if (times[1]) string = string + "下午";
+            if (times[2]) string = string + "晚上";
             string = string + "</td><td>" + '<input type="button" class="btn btn-info" value="修改">' +
-                '<input type="button" class="btn btn-danger del" value="删除" data-id="'+item.id+'">' +
+                '<input type="button" class="btn btn-danger del" value="删除" data-id="'+res.data.rooms[i]['id']+'">' +
                 "</td></tr>";
             var $tr = $(string);
             //把创建出来的$tr追加到tbody中去.
